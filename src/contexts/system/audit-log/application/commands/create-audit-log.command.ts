c97699b19ccca93b30f2db4802bea911ea18d253
Tran Hoang Anh Tu <trananhtu1112003@gmail.com>
2025-12-24 23:56:32 +0700

    fix: update imports in audit-log module after dto move

--- src/contexts/system/audit-log/application/commands/create-audit-log.command.ts
+++ src/contexts/system/audit-log/application/commands/create-audit-log.command.ts
@@ -1,4 +1,4 @@
-import { CreateAuditLogDto } from '../../dto/create-audit-log.dto';
+import { CreateAuditLogDto } from '../../presentation/dto/create-audit-log.dto';
 
 export class CreateAuditLogCommand {
   constructor(public readonly dto: CreateAuditLogDto) {}
